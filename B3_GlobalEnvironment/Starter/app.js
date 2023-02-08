4c690d9e742f542a1d7e0a82a18432ebaa8ccadc
Eric Martinez <em845@nau.edu>
2023-02-07 21:38:11 -0500

    studied

--- B3_GlobalEnvironment/Starter/app.js
+++ B3_GlobalEnvironment/Starter/app.js
@@ -1,7 +1,12 @@
-function b() {
-
+function solution(number){
+  let sumOfNum = 0
+  for (let i = number - 1; 0 < i; i--  ) {  
+    if (i % 3 === 0 || i % 5 === 0) {
+      sumOfNum += i
+    }
+  }
+  return sumOfNum
 }
 
-function a() {
-  b()
-}
+console.log(solution(-1))
+
